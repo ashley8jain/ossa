b9f06afe1a8df8785158d985738c591ddb68f6b3
Ashley Jain <ashley8jain@gmail.com>
2017-12-12 01:52:38 +0530

    facebook sdk & fbAuth added

--- app/screen/FifthScreen.js
+++ app/screen/FifthScreen.js
@@ -1,7 +1,9 @@
 import React,{ Component } from 'react'
-import {Text,View} from 'react-native'
+import {Text,View,StyleSheet} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import {LoginManager,AccessToken} from 'react-native-fbsdk'
 
+import {LoginButton} from 'react-native-fbsdk'
 
 export default class FifthScreen extends Component {
   static navigationOptions = {
@@ -11,9 +13,37 @@ export default class FifthScreen extends Component {
 
   render() {
     return(
-      <View>
+      <View style={styles.container}>
         <Text>FifthScreen</Text>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
