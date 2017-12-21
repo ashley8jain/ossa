8a3e9c5be06844a2d793d6437b2d0ea78749901a
Ashley Jain <ashley8jain@gmail.com>
2017-12-21 21:41:17 +0530

    grid image size fixed

--- app/screen/Home.js
+++ app/screen/Home.js
@@ -9,7 +9,6 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 import ImagePicker from 'react-native-image-crop-picker';
 import {GraphRequestManager,GraphRequest} from 'react-native-fbsdk'
 
-var sWidth = Dimensions.get("screen").width/3.0-10;
 
 class App extends Component {
 
@@ -31,15 +30,16 @@ class App extends Component {
       isTapped:false,
       rawData: []
     };
-    this.handleTaps = this.handleTaps.bind(this);
     this.pressView = this.pressView.bind(this);
     this.pickSingle = this.pickSingle.bind(this);
   }
+
 
   componentDidMount() {
     this.fetchData();
   }
 
+  //loads media from endpoint API
   fetchData() {
     if(this.props.screenProps.instaToken!=null){
       fetch('https://api.instagram.com/v1/users/self/?access_token='+this.props.screenProps.instaToken)
@@ -67,10 +67,6 @@ class App extends Component {
           alert('Error fetching data: ' + JSON.stringify(error));
         } else {
           console.log(result);
-          // alert(JSON.stringify(result));
-          // ref:- https://developers.facebook.com/docs/instagram-api/reference/user/#insights
-          // ref2:- https://developers.facebook.com/docs/instagram-api/reference/media/#metadata
-          // media and insight datas
           this.setState({
             rawData: result.media.data,
             dataSource: this.state.dataSource.cloneWithRows(result.media.data),
@@ -95,31 +91,8 @@ class App extends Component {
       // Start the graph request.
       new GraphRequestManager().addRequest(infoRequest).start()
     }
-
-
-
   }
 
-  render() {
-
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this.renderData.bind(this)}
-          contentContainerStyle={styles.list}
-        />
-        <BottomBar tapped = {this.state.isTapped}/>
-        <View style={{position: 'absolute',left: 0,right: 0,bottom: 0,width:'100%'}}>
-          <Button onPress={() => this.pickSingle(true)} title="Upload Image" color="#841584" accessibilityLabel="Learn more about this purple button"/>
-        </View>
-      </View>
-    );
-  }
 
   renderLoadingView() {
     return (
@@ -131,12 +104,29 @@ class App extends Component {
     );
   }
 
-  handleTaps(e){
-    !this.state.isTapped ? this.setState({isTapped:true}) : this.setState({isTapped:false}) ;
-    // ToastAndroid.show("Hello", ToastAndroid.SHORT);
+  render() {
+
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <View style={styles.statusbar}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderData.bind(this)}
+          contentContainerStyle={styles.list}
+        />
+        <View style={{position: 'absolute',left: 0,right: 0,bottom: 0,width:'100%'}}>
+          <Button onPress={() => this.pickSingle(true)} title="Upload Image" color="#841584"/>
+        </View>
+      </View>
+    );
   }
 
-  pressView(e,data){
+
+
+  pressView(data){
     // ToastAndroid.show(item.name, ToastAndroid.SHORT);
     this.props.navigation.navigate('Profile',{data});
   }
@@ -189,68 +179,17 @@ class App extends Component {
       img_url = data.media_url
     }
     return (
-
-      <TouchableWithoutFeedback onPress={(e)=>this.pressView(e,data)}>
+      <TouchableWithoutFeedback onPress={()=>this.pressView(data)}>
         <View style={styles.piccontainer}>
           <Image
             source={{uri: img_url}}
             style={styles.thumbnail}
           />
-          {/*<View style={styles.rightContainer}>
-            <Text style={styles.title}>{data.likes.count}</Text>
-            <Text style={styles.year}>{data.comments.count}</Text>
-          </View>*/}
         </View>
       </TouchableWithoutFeedback>
     );
   }
 
-  renderData2(data) {
-    // console.log("RAINBOW SIX SIEGE RAINBOW SIX SIEGE RAINBOW SIX SIEGE RAINBOW SIX SIEGE ");
-    // console.log(data);
-    return (
-      <TouchableWithoutFeedback onPress={(e)=>this.pressView(e,data)}
-                       // onPress={(e)=>this.handleTaps(e)}
-                     >
-      <View style={styles.piccontainer}>
-        <Image
-          source={{uri: data.images.thumbnail.url}}
-          style={styles.thumbnail}
-        />
-        {/*<View style={styles.rightContainer}>
-          <Text style={styles.title}>{data.likes.count}</Text>
-          <Text style={styles.year}>{data.comments.count}</Text>
-        </View>*/}
-      </View>
-      </TouchableWithoutFeedback>
-    );
-  }
-
-  // rendernet() {
-  //   return (
-  //     <WebView
-  //       source={{uri: 'https://www.instagram.com/'+this.state.username+'/' }}
-  //       style={{marginTop: 20}}
-  //     />
-  //   );
-  // }
-
-}
-
-function BottomBar(props){
-    if(!props.tapped){
-        return null;
-    }
-     return(
-        <View style={{position: 'absolute',
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'white',
-            height: '10%',
-            width:'100%'}}>
-        </View>
-    );
 }
 
 
@@ -280,61 +219,6 @@ class OpenImage extends Component{
     componentWillMount(){
       process.nextTick = setImmediate;
 
-      var app = new Clarifai.App({
-        apiKey: 'b13eedefd5ee4207b1aab989b21930b4'
-      });
-
-      // app.models.predict(Clarifai.GENERAL_MODEL, this.props.navigation.state.params.data.images.standard_resolution.url)
-      // .then(response =>  {
-      //     console.log("abcbdjkfkajsndjksandjkas");
-      //     //console.log(JSON.stringify(response.outputs[0].data.concepts, null, 2));
-      //     this.setState({
-      //       tags: response.outputs[0].data.concepts
-      //     });
-      //
-      //     console.log("abcbdjkfkajsndjksandjkas");
-      //     console.log(this.state.tags);``
-      //   },
-      //   function(err) {
-      //     console.error(err);
-      //     console.log("ERROR ERROR ERROR")
-      //     }
-      //   );
-
-    }
-
-    render1(){
-      return(
-        //    <View style={styles.textContainer}>
-        //        <Text style={{fontSize:30}}>Name: {this.props.navigation.state.params.item.name} </Text>
-        //        <Text style={{fontSize:30}}>id: {this.props.navigation.state.params.item.id} </Text>
-        //    </View>
-        <View style = {{flexDirection:'column', flex:1, justifyContent: 'center',
-              alignItems: 'center', backgroundColor: 'white'}}>
-              <View style={{width: '100%',flex:1, backgroundColor: 'black', justifyContent: 'flex-end', alignItems:'center'}}>
-                  <Image
-                      source={{uri: this.props.navigation.state.params.data.images.standard_resolution.url}}
-                      /*style={{width:this.props.navigation.state.params.data.images.standard_resolution.width,
-                              height:this.props.navigation.state.params.data.images.standard_resolution.height,
-                              }}*/
-                      style={styles.thumbnail2}
-                  />
-              </View>
-
-              <View style={styles.rightContainer}>
-                <ScrollView >
-                {
-                  this.state.tags.map((item, index) => (
-                  <View key = {item.id} style = {styles.item2}>
-                    <Text style = {{fontSize:20}}>{item.name}</Text>
-                    <Text style = {{fontSize:20}}>{item.value}</Text>
-                  </View>
-                  ))
-                }
-                </ScrollView>
-              </View>
-        </View>
-      )
     }
 
     render(){
@@ -492,6 +376,16 @@ const styles = StyleSheet.create ({
       height:'27%',
       backgroundColor: 'white'
    },
+   statusbar:
+   {
+       flex: 1,
+       ...Platform.select({
+         ios: {
+            paddingTop: 20
+          },
+          android: {}
+        }),
+   },
   item: {
     width:'32%',
     height: 100,
@@ -523,49 +417,21 @@ const styles = StyleSheet.create ({
     alignItems: 'center'
   },
   piccontainer: {
-    backgroundColor: '#EEEEEE',
-    padding: 5,
-    margin: 1,
-    width:'32.5%'
-  },
-  piccontainers: {
-    backgroundColor: 'white',
-    width: sWidth,
-    height:sWidth,
-    padding: 5,
-    margin: 1,
-  },
-  piccontainer2: {
-    width: '100%',flex:1, backgroundColor: 'black'
-  },
-  rightContainer: {
-    flex:1,
-    backgroundColor: 'black',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    color:'white',
-  },
-  year: {
-    textAlign: 'center',
-    color:'white',
+    backgroundColor: '#ffffff',
+    padding: 1,
+    width:'33.3%',
+    aspectRatio: 1
   },
   thumbnail: {
-    width: 150,
-    height: 150,
+    width:'100%',
+    height: '100%'
   },
   thumbnailCaption: {
     width: 125,
     height: 125,
   },
-  thumbnail2: {
-    width:400,
-    height:400
-  },
   list: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#ffffff',
     flexDirection: 'row',
     flexWrap: 'wrap'
   },
